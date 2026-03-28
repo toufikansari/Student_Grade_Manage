c2f4340bd1d440ae83925088961f6b45581651e2
toufikansari <toufikmdansari@gmail.com>
2026-03-28 21:36:28 +0530

    Delete Function

--- Student_Grade/Project.py
+++ Student_Grade/Project.py
@@ -3,3 +3,30 @@ Student_grade = {  }
 
 
 #  Adding Element
+def add_Student(name,grade):
+    Student_grade[name] = grade
+    #[Aatif] = 100
+    print(f"Added {name} With a {grade}")
+    #Added sugar with a 100
+
+
+#Update a Student
+def update_Student(name, grade):
+    if name in Student_grade:
+        Student_grade[name] = grade
+        print(f"Added {name} With Marks are updated {grade}")
+    else:
+        print(f"{name} is not found !")
+
+
+# Delete Student
+
+def delete_student(name):
+    if name in Student_grade:
+        del Student_grade[name]
+        print(f"{name} has been succesfully deleted")
+    else:
+        print(f"{name} is not found !")
+
+
+# View All student
